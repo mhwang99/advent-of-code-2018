edca2dc312ab09c219a28e8659f7292d614cb51f
Mark Hwang <mhwang@serenova.com>
2018-12-06 09:38:25 -0400

    day 6

--- src/aoc18/d6.clj
+++ src/aoc18/d6.clj
@@ -3,8 +3,13 @@
             [aoc18.core :as aoc :refer :all]))
 
 
-(defn dif [a b] (if (> a b) (- a b) (- b a)))
-(defn dif2 [[ax ay] [bx by]] (+ (dif ax bx) (dif ay by)))
+(defn dif
+  [a b]
+  (if (number? a)
+    (if (> a b) (- a b) (- b a))
+    (let [[ax ay] a
+          [bx by] b]
+      (+ (dif ax bx) (dif ay by)))))
 
 (defn get-nth [l i]
   [(vec (concat (take i l) (drop (inc i) l))) (nth l i)])
@@ -17,25 +22,26 @@
        (some (fn [[x y]] (and (> y iy) (<= (dif x ix) (dif y iy)))) l)))
 
 (defn get-grid-count
-  [l [ix iy]]
+  [l [ix iy] fn-check]
   (loop [s #{}
-         ps [[[ix iy] 0]]
-         ret 0]
-    (let [[ps n s] (reduce (fn [[ps n s] [[ix iy] len]]
+         ps #{[ix iy]}
+         ret 0
+         len 0]
+    (let [[ps n s] (reduce (fn [[ps n s] [ix iy]]
                              (if (and (not (s [ix iy]))
-                                      (not-any? #(<= (dif2 % [ix iy]) len) l))
+                                      (fn-check l [ix iy] len))
                                [(conj ps
-                                      [[(inc ix) iy] (inc len)]
-                                      [[(dec ix) iy] (inc len)]
-                                      [[ix (inc iy)] (inc len)]
-                                      [[ix (dec iy)] (inc len)])
+                                      [(inc ix) iy]
+                                      [(dec ix) iy]
+                                      [ix (inc iy)]
+                                      [ix (dec iy)])
                                 (inc n)
                                 (conj s [ix iy])]
                                [ps n (conj s [ix iy])]))
                            [[] 0 s] ps)]
       (if (= n 0)
-        (do  ret)
-        (recur s ps (+ ret n))))))
+        ret
+        (recur s ps (+ ret n) (inc len))))))
 
 (defn q1 [ll]
   (->> (range (count ll))
@@ -43,9 +49,18 @@
                (let [[l p] (get-nth ll i)]
                  (if-not (limited? l p)
                    0
-                   (get-grid-count l p)))))
+                   (get-grid-count l p
+                                   (fn [l p len]
+                                     (not-any? #(<= (dif % p) len) l)))))))
        (apply max)))
 
+(defn q2 [ll mx]
+  (let [ax (->> (mapv first ll) (apply +) (#(/ % (count ll))) int)
+        ay (->> (mapv last ll) (apply +) (#(/ % (count ll))) int)]
+    (get-grid-count ll [ax ay]
+                    (fn [l p _]
+                      (< (apply + (mapv #(dif % p) l)) mx)))))
+
 (def in (get-line-num))
-(def sample [[1 1] [1 6] [8 3] [3 4] [5 5] [8 9]])
 (q1 in)
+(q2 in 10000)
