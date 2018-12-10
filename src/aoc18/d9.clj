6c430cd2b29367c204cad402ee913cd770765681
Mark Hwang <mhwang@liveopscloud.com>
2018-12-10 10:24:15 -0400

    day 9

--- src/aoc18/d9.clj
+++ src/aoc18/d9.clj
@@ -4,50 +4,39 @@
 (defprotocol INode
   (prv [this])
   (nxt [this])
-  (vol [this])
-  (set-pn [this p n])
   (set-prev [this node])
   (set-next [this node]))
-
-(defn setv [l idx f]
-  (swap! (get l idx) f))
 
 (deftype Node [value ^{:volatile-mutable true} prev-node ^{:volatile-mutable true} next-node]
   INode
   (prv [_] prev-node)
   (nxt [_] next-node)
-  (vol [_] value)
-  (set-pn [_ p n] (set! prev-node p) (set! next-node n))
   (set-prev [_ node] (set! prev-node node))
   (set-next [_ node] (set! next-node node)))
 
 (defn q0 [[e m]]
-  (let [a (Node. 0 nil nil)
-        b (Node. 2 nil nil)
-        c (Node. 1 nil nil)
-        _ (do (set-pn a c b)
-              (set-pn b a c)
-              (set-pn c b a))
-        score (mapv #(atom %) (repeat e 0))]
-    (loop [c b
-           n 2
-           idx 2]
+  (let [c (Node. 0 nil nil)
+        score (zipmap (range e) (repeat 0))]
+    (set-prev c c)
+    (set-next c c)
+    (loop [c c
+           n 0
+           score score]
       (if (>= n m)
-        (apply max (map deref score))
+        (apply max (vals score))
         (if (= 0 (mod (inc n) 23))
           (let [c (-> c   prv prv prv   prv prv prv   prv)]
-            (setv score idx #(+ % 1 (vol c) n))
             (set-next (-> c prv) (-> c nxt))
             (set-prev (-> c nxt) (-> c prv))
-            (recur (nxt c)  (inc n) (mod (inc idx) e)))
+            (recur (nxt c) (inc n)
+                   (update score (mod n e) #(+ % (.value c) (inc n)))))
           (let [nc (Node. (inc n) (-> c nxt) (-> c nxt nxt))]
             (set-next (-> nc prv) nc)
             (set-prev (-> nc nxt) nc)
-            (recur nc (inc n) (mod (inc idx) e))))))))
+            (recur nc (inc n) score)))))))
 
 (defn q1 [l] (q0 l))
-(defn q2 [l] (-> l
-                 (update 1 #(* 100 %))
-                 q0))
+(defn q2 [l] (q0 (update l 1 #(* 100 %))))
+
 #_(time (q1 (get-num)))
 #_(time (q2 (get-num)))
