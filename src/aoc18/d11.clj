c7487df9b225d9d7adc06436862dbe88302335b9
Mark Hwang <mhwang@liveopscloud.com>
2018-12-11 14:13:35 -0400

    day 11

--- src/aoc18/d11.clj
+++ src/aoc18/d11.clj
@@ -3,7 +3,7 @@
 
 (def get-power
   (memoize
-    (fn [x y serial size]
+    (fn [serial x y size]
       (if (= size 1)
         (-> (+ x 10)
             (* y)
@@ -13,24 +13,26 @@
             (mod 10)
             (- 5))
         (let [size1 (int (/ size 2))
-              size2 (- size size1)]
-          (+ (get-power x y serial size1)
-             (get-power (+ x size2) (+ y size2) serial size1)
-             (get-power (+ x size1) y serial size2)
-             (get-power x (+ y size1) serial size2)
+              size2 (- size size1)
+              f (partial get-power serial)]
+          (+ (f x y size1)
+             (f (+ x size2) (+ y size2) size1)
+             (f (+ x size1) y size2)
+             (f x (+ y size1) size2)
              (if (= size1 size2) 0
-               (- 0 (get-power (+ x size1) (+ y size1) serial 1)))))))))
+               (- 0 (f (+ x size1) (+ y size1) 1)))))))))
 
 (defn q0
   [size-range serial]
   (->> (for [x (range 1 301)
              y (range 1 301)
              size size-range
-             :when (and (<= (+ x size) 301)
+             :when (and (> size 0)
+                        (<= (+ x size) 301)
                         (<= (+ y size) 301))]
          [x y size])
        (reduce (fn [ret [x y size]]
-                 (let [p (get-power x y serial size)]
+                 (let [p (get-power serial x y size)]
                    (if (> p (first ret))
                      [p [x y size]]
                      ret)))
